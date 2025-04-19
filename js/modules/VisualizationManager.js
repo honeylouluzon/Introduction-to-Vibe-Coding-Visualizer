57385995673e45bb0152743fed84a66ea106bf39
honeylouluzon <42791756+honeylouluzon@users.noreply.github.com>
2025-04-19 17:46:33 +0000

    fix

--- js/modules/VisualizationManager.js
+++ js/modules/VisualizationManager.js
@@ -4,9 +4,7 @@ export class VisualizationManager {
         this.chartContainer = document.querySelector('.chart-container'); // Reference to the chart container
         this.initializeChart();
         this.hideChart(); // Initially hide the chart
-        this.resultContainer = document.createElement('div'); // Create a container for results
-        this.resultContainer.className = 'comparison-result';
-        this.chartContainer?.parentNode?.appendChild(this.resultContainer); // Append below the chart
+        this.createResultContainer(); // Create and append the result container
     }
 
     initializeChart() {
@@ -70,6 +68,15 @@ export class VisualizationManager {
                 }
             }
         });
+    }
+
+    createResultContainer() {
+        this.resultContainer = document.createElement('div'); // Create a container for results
+        this.resultContainer.className = 'comparison-result';
+        this.resultContainer.style.display = 'none'; // Initially hidden
+        if (this.chartContainer) {
+            this.chartContainer.parentNode.insertBefore(this.resultContainer, this.chartContainer.nextSibling); // Append below the chart
+        }
     }
 
     addEntity(entity) {
